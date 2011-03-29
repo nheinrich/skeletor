671a018fa6db0c5942cdc1f1287139f8bcd45436
Neil Heinrich <neil@pixels-and-bits.com>
2011-03-28 21:12:50 -0700

    rails app

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'mysql', '~>2.8.1'
+gem 'rails', '~>3.0.5'
+gem 'haml', '~>3.1.0.alpha.147'
+gem 'compass', '~>0.11.beta.5'
+
+group :development do
+  gem 'capistrano'
+  gem 'capistrano-ext'
+end
+
+group :test do
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeletor::Application.config.secret_token = '83f5894ff9e23ac3a5fecf203d6e77c1bc2cc6f3aac27f413c12bbd558f02bacbf13ba5f7f4db606effbe393929fdbe449296f715bdb5f9cdad2b86f1e966001'
